4c934ea4111580933736d15c1a93318b1051364f
bchabot <bchabot@gmail.com>
2015-11-19 15:12:34 -0500

    Create consuite-display.php

---
+++ consuite/public/partials/consuite-display.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://github.com/bchabot/consuite/
+ * @since      0.0.1
+ *
+ * @package    consuite
+ * @subpackage consuite/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
