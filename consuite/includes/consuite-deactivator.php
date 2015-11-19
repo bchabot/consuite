5e172e5b708fecb92e20990a11c69e6197d7925a
bchabot <bchabot@gmail.com>
2015-11-19 15:58:31 -0500

    Create consuite-deactivator.php

---
+++ consuite/includes/consuite-deactivator.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/bchabot/consuite/
+ * @since      0.0.1
+ *
+ * @package    consuite
+ * @subpackage consuite/includes
+ */
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      0.0.1
+ * @package    consuite
+ * @subpackage consuite/includes
+ * @author     Brian Chabot <brian@brianchabot.org>
+ */
+class consuite_Deactivator {
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+	}
+}
