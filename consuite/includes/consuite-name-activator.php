5f6db458460ef921e3457f9cd28789eb61e3f300
bchabot <bchabot@gmail.com>
2015-11-19 15:36:53 -0500

    Create consuite-name-activator.php

---
+++ consuite/includes/consuite-name-activator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/bchabot/consuite
+ * @since      0.0.1
+ *
+ * @package    consuite
+ * @subpackage consuit/includes
+ */
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.0.1
+ * @package    consuite
+ * @subpackage consuite/includes
+ * @author     Brian Chabot <brian@brianchabot.org>
+ */
+class consuite_Activator {
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+	}
+}
+
+?>
