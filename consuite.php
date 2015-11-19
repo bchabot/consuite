35081df16210462eb256c25940cff76d9795c8c7
bchabot <bchabot@gmail.com>
2015-11-19 11:22:27 -0500

    Initial creation with headers.

---
+++ consuite.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: CON Suite
+Plugin URI:  https://github.com/bchabot/consuite
+Description: CON Suite is (intended to be) a comprehensive, free, open source, WordPress plugin for planning and managing 
+              Conventions/Conferences/Convocations/etc... "Cons"
+Version:     0.01
+Author:      Brian Chabot
+Author URI:  http://brianchabot.org
+License:     GPL3
+ 
+CON Suite is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+CON Suite is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with CON Suite. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Domain Path: /languages
+Text Domain: consuite
+*/
+
+
+
+
+?>
